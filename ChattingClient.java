ea8e757a2670be7450bd1ba240fe85b09a6669db
KSLEE7746 <kslee7746@naver.com>
2017-05-29 17:28:47 +0900

    Add files via upload

---
+++ ChattingClient.java
@@ -0,0 +1,75 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ChattingClient {
+	public static void main(String[] args) {
+		Socket socket = null;
+		BufferedReader br = null;
+		PrintWriter pw = null;
+		
+		try {
+			socket = new Socket("192.168.1.32", 10001);
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+			
+			System.out.print("접속 ID 입력 : ");
+			String id = keyboard.readLine();
+			pw.println(id);
+			pw.flush();
+			InputThread inputTh = new InputThread(socket, br);
+			inputTh.start();
+			String line = null;
+			while((line = keyboard.readLine()) != null){
+				pw.println(line);
+				pw.flush();
+				if(line.equalsIgnoreCase("/quit")) {
+					inputTh.stop();
+					break;
+				}
+			}
+			System.out.println("채팅 서버 접속 종료");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pw != null) pw.close();
+				if(br != null) br.close();
+				if(socket != null) socket.close();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
+
+class InputThread extends Thread{
+	private Socket socket;
+	private BufferedReader br = null;
+	public InputThread(Socket socket, BufferedReader br) {
+		this.socket = socket;
+		this.br = br;
+	}
+	@Override
+	public void run() {
+		String line = null;
+		try {
+			while((line = br.readLine()) != null){
+				System.out.println(line);
+				if(line.equalsIgnoreCase("/quit")) break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(br != null) br.close();
+				if(socket != null) socket.close(); 
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ ChattingServer.java
@@ -0,0 +1,95 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class ChattingServer {
+	public static void main(String[] args) {
+		try {
+			HashMap map = new HashMap<>();
+			ServerSocket socket = new ServerSocket(10001);
+			System.out.println("Server 접속자 대기중...");
+			while(true){
+				Socket client = socket.accept();
+				UserThread uThread = new UserThread(client, map);
+				uThread.start();		
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+class UserThread extends Thread{
+	private String id;
+	private Socket socket;
+	private HashMap map;
+	private BufferedReader br;
+	public UserThread(Socket socket, HashMap map) {
+		this.socket = socket;
+		this.map = map;
+		
+		try {
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			id = br.readLine();
+			broadcast(id + "님이 접속함");
+			System.out.println("접속한 사용자 ID는 [" + id + "]입니다.");
+			synchronized (map) {
+				map.put(this.id, pw);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void run() {
+		String line = null;
+		try {
+			while((line = br.readLine()) != null){
+				if(line.equalsIgnoreCase("/quit")) break;
+				if(line.indexOf("/to ") == 0) sendmsg(line);
+				else broadcast("[" + id + "] : " + line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			synchronized (map) {
+				map.remove(id);
+			}
+			broadcast(id+"님이 접속 종료함");
+			try{
+				if(socket != null) socket.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void sendmsg(String msg){
+		int start = msg.indexOf(" ")+1;
+		int end = msg.indexOf(" ", start);
+		if(end != -1){
+			String to = msg.substring(start, end);
+			String sendMsg = msg.substring(end+1);
+			Object obj = map.get(to);
+			if(obj != null){
+				PrintWriter pw = (PrintWriter) obj;
+				pw.println(id+"님이 보낸 내용 : " + sendMsg);
+				pw.flush();
+			}
+		}
+	}
+	public void broadcast(String msg){
+		synchronized (map) {
+			Iterator it = map.values().iterator();
+			while(it.hasNext()){
+				PrintWriter pw = (PrintWriter) it.next();
+				pw.println(msg);
+				pw.flush();
+			}
+		}
+	}	
+}
